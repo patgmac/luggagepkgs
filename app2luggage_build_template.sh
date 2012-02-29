12c78ca718f5dc5b6708f07cd10bf5bdf72652be
Patrick Gallagher <patgmac+git@gmail.com>
2012-02-28 19:48:41 -0500

    app2luggage script template

---
+++ app2luggage_build_template.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+
+## app2luggage_build_template.sh ##
+# Patrick Gallagher <patrick@macadmincorner.com>
+
+# Bash script to create a Luggage pkg and makefile with app2luggage
+
+# Enter app name with no spaces
+appname=VLC
+
+# Application name as it appears in your /Applications folder. Include .app.
+app=VLC.app
+
+# Change to your reverse domain, ie. com.companyname
+reverse_domain=edu.emory
+
+# Location where you want to store your pkg and makefile
+dirname=~/src/luggagepkgs/VLC
+
+# script action. Shouldn't need to edit this
+sudo app2luggage.rb --application /Applications/${app} --directory-name ${dirname} --package-id ${appname} --reverse-domain ${reverse_domain} --make-pkg --make-dmg
+sudo chmod 775 ${dirname}
+
+exit 0
